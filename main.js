f2b491b37f6206e04eedfc03ca3a829e503fb45d
Bastien Girardet <bastien.girardet@gmail.com>
2018-02-24 19:14:51 +0100

    Well done guys

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-function touch(self){
-    console.log(self);
-    self.setAttribute("style", "background-color: red;"); 
-    console.log("test");
+var preferences = new Object();
+    preferences.single = 0;
+    preferences.family = 0;
+    preferences.couple = 0;
+    preferences.sport = 0;
+    preferences.nature = 0;
+    preferences.monuments = 0;
+    preferences.food = 0;
+
+    
+
+function includePref(pref){
+    
+    console.log(pref);
+    
+    if (document.getElementById(pref).className == "selected circle"){
+        document.getElementById(pref).className = "circle";
+        preferences[pref] = 0;
+    }
+    else{
+        document.getElementById(pref).className = "selected circle";
+        preferences[pref] = 1;
+    }
 }
+  
