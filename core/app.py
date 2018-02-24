22f48a77c5e504efe188aa22980bcb167116e621
Bastien Girardet <bastien.girardet@gmail.com>
2018-02-24 22:56:44 +0100

    updated core python

--- core/app.py
+++ core/app.py
@@ -4,14 +4,7 @@
 from flask import Flask
 from flask import request
 from flask import json
-<<<<<<< HEAD
 from flask_cors import CORS
-=======
-from flask import jsonify
-import path
-
-
->>>>>>> 41f7d4cfbedeb1917d834dce66ae08dbeb67224f
 app = Flask(__name__)
 CORS(app)
 
@@ -22,13 +15,4 @@ def hello():
 
 @app.route("/inputs", methods=["POST"])
 def fetchInputs():
-<<<<<<< HEAD
     return request.get_data() 
-=======
-    if request.headers['Content-Type'] == 'application/json':
-        answer = {
-            "test":"Test2",
-            "test3": 6
-        }
-        return json.jsonify(answer)
->>>>>>> 41f7d4cfbedeb1917d834dce66ae08dbeb67224f
